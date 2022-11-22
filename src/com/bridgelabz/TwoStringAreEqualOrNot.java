d8ed7fcfdbebad48f4503bc33a3bf35ab5104f2e
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-22 21:11:38 +0530

    Check two strings are equal or not

---
+++ src/com/bridgelabz/TwoStringAreEqualOrNot.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class TwoStringAreEqualOrNot {
+    public static void main(String[] args) {
+        
+        String stringOne = "String";
+
+        String stringTwo = "String";
+
+        if (stringOne==stringTwo){
+
+            System.out.println(stringOne+" and "+stringTwo+" are equal");
+        }
+        else {
+
+            System.out.println(stringOne+" and "+stringTwo+" are not equal");
+        }
+    }
+}
