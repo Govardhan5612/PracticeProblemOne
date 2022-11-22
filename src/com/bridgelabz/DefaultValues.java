550c5d7e57aa1bdb77da02fd5f4c039dfc6f0095
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-22 21:09:46 +0530

    Print default values

---
+++ src/com/bridgelabz/DefaultValues.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+public class DefaultValues {
+    static byte byteDefaultValue;
+    static short shortDefaultValue;
+    static int intDefaultValue;
+    static long longDefaultValue;
+    static float floatDefaultValue;
+    static double doubleDefaultValue;
+    static boolean booleanDefaultValue;
+    static char charDefaultValue;
+
+    public static void main(String[] args) {
+        System.out.println("byte default value : "+byteDefaultValue);
+
+        System.out.println("short default value : "+shortDefaultValue);
+
+        System.out.println("int default value : "+intDefaultValue);
+
+        System.out.println("long default value : "+longDefaultValue);
+
+        System.out.println("float default value : "+floatDefaultValue);
+
+        System.out.println("double default value : "+doubleDefaultValue);
+
+        System.out.println("boolean default value : "+booleanDefaultValue);
+
+        System.out.println("char default value : "+charDefaultValue);
+    }
+}
+
